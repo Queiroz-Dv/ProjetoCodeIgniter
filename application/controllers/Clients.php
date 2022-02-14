47f2fbabae2be25e82761f85f77d3b07b92ddafa
Queiroz <kalifor_dbel@hotmail.com>
2022-02-14 15:28:23 -0300

    Implementing Update's client method

--- application/controllers/Clients.php
+++ application/controllers/Clients.php
@@ -27,9 +27,55 @@ class Clients extends CI_Controller
       ),
       'clients' => $this->CoreModel->GetAll('clients'),
     );
-
     $this->load->view('layout/header', $data);
     $this->load->view('clients/index');
     $this->load->view('layout/footer');
+  }
+
+  public function edit($clients_id = null)
+  {
+    if (!$clients_id || !$this->CoreModel->GetById('clients', array('clients_id' => $clients_id))) {
+      $this->session->flashdata('error', 'Client is not found');
+      redirect('clients');
+    } else {
+      $this->form_validation->set_rules('clients_first_name', '', 'trim|required|min_length[4]|max_length[45]');
+      $this->form_validation->set_rules('clients_last_name', '', 'trim|required|min_length[4]|max_length[145]');
+      $this->form_validation->set_rules('clients_birthday', '', 'required');
+      $this->form_validation->set_rules('clients_birthday', '', 'trim|required|min_length[4]|max_length[145]');
+      $this->form_validation->set_rules('clients_cpf_cnpj', '', 'trim|required|exact_length[18]');
+      $this->form_validation->set_rules('clients_rg_ie', '', 'trim|required|max_length[20]');
+      $this->form_validation->set_rules('clients_email', '', 'trim|required|valid_email|max_length[20]');
+      $this->form_validation->set_rules('clients_telephone', '', 'trim|max_length[14]');
+      $this->form_validation->set_rules('clients_phone', '', 'trim|max_length[15]');
+      $this->form_validation->set_rules('clients_zip_code', '', 'trim|required|exact_length[9]');
+      $this->form_validation->set_rules('clients_address', '', 'trim|max_length[155]');
+      $this->form_validation->set_rules('clients_number', '', 'trim|required|max_length[20]');
+      $this->form_validation->set_rules('clients_district', '', 'trim|required|max_length[45]');
+      $this->form_validation->set_rules('clients_complement', '', 'trim|max_length[145]');
+      $this->form_validation->set_rules('clients_city', '', 'trim|required|max_length[50]');
+      $this->form_validation->set_rules('clients_state', '', 'trim|required|exact_length[2]');
+      $this->form_validation->set_rules('clients_obs', '', 'max_length[500]');
+
+      if ($this->form_validation->run()) {
+        echo '<pre>';
+        print_r($this->input->post());
+        exit();
+      } else {
+        $data = array(
+          'title' => 'Update Clients',
+          'scripts' => array(
+            'vendor/mask/jquery.mask.min.js',
+            'vendor/mask/app.js',
+          ),
+          'client' => $this->CoreModel->GetById('clients', array('clients_id' => $clients_id))
+        );
+        echo '<pre>';
+        print_r($data['client']);
+        exit();
+        $this->load->view('layout/header', $data);
+        $this->load->view('clients/edit');
+        $this->load->view('layout/footer');
+      }
+    }
   }
 }
